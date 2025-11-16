08a3bd6a9a86be64a374c8ac8e5158e62d9310dc
Nulligma <shantanu@nulligma.com>
2025-11-16 05:33:52 +0530

    updated strapi

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,33 +1,12 @@
 export default [
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "http://localhost:3000"],
-          "media-src": ["'self'", "data:", "blob:"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["http://localhost:3000", "http://localhost:3001"],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
